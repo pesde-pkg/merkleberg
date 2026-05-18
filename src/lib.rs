2bd3d3cc5826292bca40e89b5f962868ff4f794b
Erica Marigold <hi@devcomp.xyz>
2026-05-18 15:32:22 +0100

    style: format `lib.rs` using `rustfmt`

--- src/lib.rs
+++ src/lib.rs
@@ -11,12 +11,12 @@ mod tests;
 pub mod util;
 
 pub use error::{Error, Result};
-pub use helper::{leaf_index_to_mmr_size, leaf_index_to_pos};
 #[cfg(feature = "sha2")]
 pub use helper::hash_pospair64;
-pub use merge::{Merge, MergeMMRIVER};
+pub use helper::{leaf_index_to_mmr_size, leaf_index_to_pos};
 #[cfg(feature = "sha2")]
 pub use merge::Sha256Merge;
+pub use merge::{Merge, MergeMMRIVER};
 pub use mmr::{MMR, MerkleProof};
 pub use mmr_store::{MMRStoreReadOps, MMRStoreWriteOps};
 pub use mmriver::{ConsistencyProof, InclusionProof, MMRIVER, included_root};
